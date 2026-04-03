81778697a277b2e68681df95538c673814a94969
brentfpage <brentfpage@gmail.com>
2026-04-02 23:35:00 -0700

    remove obsolete selector_ui code

--- app/src/main/cpp/selector_ui.cpp
+++ app/src/main/cpp/selector_ui.cpp
@@ -2,38 +2,6 @@
 #include "imgui.h"
 #include "selector_ui.h"
 
-void selectorUI::draw(float width_pixels, inputsUI* inputs_ui)
-{
-    ImGuiStyle& style = ImGui::GetStyle();
-    ImGui::BeginGroup();
-    bool need_pop = false;
-    ImGui::PushID("selector ui");
-    int n_disabled;
-    for(int i=0; i < (n_tiles-1); i++) {
-        if(!tiles[i]->is_expanded) {
-            n_disabled++;
-            if(ImGui::Button(tiles[i]->short_name)) {
-                tiles[i]->is_expanded = true;
-                n_disabled--;
-            }
-        }
-        if(width == UI_tile::Width::duplex) {
-            if(((i%2)==0) && (i<(n_tiles-2))) {
-                int free_space = width_pixels - ImGui::CalcTextSize(tiles[i]->short_name).x - ImGui::CalcTextSize(tiles[i+1]->short_name).x - 2 * CHECKBOX_SIZE - 2 * style.ItemInnerSpacing.x - style.FramePadding.x * 2;
-                ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing,ImVec2(free_space,0.f));
-                need_pop = true;
-                ImGui::SameLine();
-            } else if (need_pop) {
-                ImGui::PopStyleVar();
-                need_pop = false;
-            }
-        }
-    }
-    n_lines = n_disabled / ((width == UI_tile::Width::duplex) + 1);
-    ImGui::PopID();
-    ImGui::EndGroup();
-}
-
 void selectorUI::draw_popup()
 {
     if(ImGui::BeginPopup("config_settings")) {
@@ -56,5 +24,5 @@ void selectorUI::draw_popup()
 int selectorUI::get_height()
 {
     ImGuiStyle& style = ImGui::GetStyle();
-    return CHECKBOX_SIZE * n_lines + style.ItemSpacing.y * (n_lines - 1) + style.WindowPadding.y*2 + 2 * style.ItemSpacing.y + style.SeparatorSize + ImGui::GetFontSize();
+    return CHECKBOX_SIZE * n_tiles + style.ItemSpacing.y * (n_tiles - 1) + style.WindowPadding.y*2 + 2 * style.ItemSpacing.y + style.SeparatorSize + ImGui::GetFontSize();
 }
--- app/src/main/cpp/selector_ui.h
+++ app/src/main/cpp/selector_ui.h
@@ -2,14 +2,13 @@
 #define SELECTORUI_H
 
 #include "ui_tile.h"
-class selectorUI : public UI_tile
+class selectorUI
 {
     int n_tiles;
     UI_tile** tiles;
 public:
-    selectorUI(UI_tile** tiles, int n_tiles) : tiles(tiles), n_tiles(n_tiles), UI_tile("Selector", "Sel.", UI_tile::Width::single, 6) {};
-    void draw(float width_pixels, inputsUI* inputs_ui = nullptr) override;
-    int get_height() override;
+    selectorUI(UI_tile** tiles, int n_tiles) : tiles(tiles), n_tiles(n_tiles) {};
+    int get_height();
     void draw_popup();
 };
 
