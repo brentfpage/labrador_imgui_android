24e1c37d369e6e652e9757dda8c5399accc3857f
brentfpage <brentfpage@gmail.com>
2026-04-05 09:33:44 -0700

    add reference lines by tapping and/or dragging on an axis

--- app/src/main/cpp/plot_ui.cpp
+++ app/src/main/cpp/plot_ui.cpp
@@ -1,4 +1,5 @@
 #include "implot.h"
+#include "imgui_internal.h"
 #include "plot_ui.h"
 #include "librador.h"
 
@@ -75,6 +76,13 @@ void plotUI::draw(bool iso_thread_active, inputsUI::Mode mode, bool chA_enabled,
         from_librador_chB = &blank_data;
         time_array = blank_data;
     }
+    static double xval1 = -.06;
+    static double xval2 = -.12;
+    static double yval1 = -.06;
+    static double yval2 = -.12;
+    static bool cursor_drag_tool_toggle = false;
+    static bool enable_xcursors = false;
+    static bool enable_ycursors = false;
 
     ImGui::BeginChild("plot",ImVec2(data_width, plot_height));
     {
@@ -98,8 +106,58 @@ void plotUI::draw(bool iso_thread_active, inputsUI::Mode mode, bool chA_enabled,
                 if(chB_enabled)
                     ImPlot::PlotLine("CH B", time_array.data(), from_librador_chB->data(), from_librador_chB->size(), spec);
 //             }
-            ImPlotRect axes_limits = ImPlot::GetPlotLimits();
 
+            ImGuiContext& g = *GImGui;
+
+            if(ImPlot::IsAxisActivated(ImAxis_X1)) {
+                float mouse_down_clicked_val = ImPlot::getMouseDownClickedVal(ImAxis_X1);
+                cursor_drag_tool_toggle = fabs(xval1 - mouse_down_clicked_val) > fabs(xval2 - mouse_down_clicked_val);
+            }
+
+            ImPlotDragToolFlags x1_drag_tool_flags = cursor_drag_tool_toggle ? ImPlotDragToolFlags_NoAxisInputs : 0;
+            ImPlotDragToolFlags x2_drag_tool_flags = cursor_drag_tool_toggle ? 0 : ImPlotDragToolFlags_NoAxisInputs;
+
+            if(ImPlot::IsAxisClicked(ImAxis_X1)) {
+                if(!enable_xcursors) {
+                    xval1 = ImPlot::getMouseDownClickedVal(ImAxis_X1);
+                    xval2 = ImPlot::getMouseDownClickedVal(ImAxis_X1);
+                }
+                enable_xcursors = true;
+                if(g.IO.MouseClickedLastCount[0]==2) {
+                    enable_xcursors = false;
+                }
+            }
+
+            if(enable_xcursors) {
+                ImPlot::DragLineX(0,&xval1,ImVec4(1,1,1,1), 1.f, x1_drag_tool_flags);
+                ImPlot::DragLineX(1,&xval2,ImVec4(1,1,1,1), 1.f, x2_drag_tool_flags);
+            }
+
+            if(ImPlot::IsAxisActivated(ImAxis_Y1)) {
+                float mouse_down_clicked_val = ImPlot::getMouseDownClickedVal(ImAxis_Y1);
+                cursor_drag_tool_toggle = fabs(yval1 - mouse_down_clicked_val) > fabs(yval2 - mouse_down_clicked_val);
+            }
+
+            ImPlotDragToolFlags y1_drag_tool_flags = cursor_drag_tool_toggle ? ImPlotDragToolFlags_NoAxisInputs : 0;
+            ImPlotDragToolFlags y2_drag_tool_flags = cursor_drag_tool_toggle ? 0 : ImPlotDragToolFlags_NoAxisInputs;
+
+            if(ImPlot::IsAxisClicked(ImAxis_Y1)) {
+                if(!enable_ycursors) {
+                    yval1 = ImPlot::getMouseDownClickedVal(ImAxis_Y1);
+                    yval2 = ImPlot::getMouseDownClickedVal(ImAxis_Y1);
+                }
+                enable_ycursors = true;
+                if(g.IO.MouseClickedLastCount[0]==2) {
+                    enable_ycursors = false;
+                }
+            }
+
+            if(enable_ycursors) {
+                ImPlot::DragLineY(0,&yval1,ImVec4(1,1,1,1), 1.f, y1_drag_tool_flags);
+                ImPlot::DragLineY(1,&yval2,ImVec4(1,1,1,1), 1.f, y2_drag_tool_flags);
+            }
+
+            ImPlotRect axes_limits = ImPlot::GetPlotLimits();
             ymin = axes_limits.Y.Min;
             ymax = axes_limits.Y.Max;
             xmin = axes_limits.X.Min;
